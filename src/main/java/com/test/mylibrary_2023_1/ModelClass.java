b2e61063bd7dec2ac2971a0480cf1279e8e6256a
haider <haiderqadir026@gmail.com>
2023-05-25 14:23:40 +0500

    first commit

---
+++ src/main/java/com/test/mylibrary_2023_1/ModelClass.java
@@ -0,0 +1,13 @@
+package com.test.mylibrary_2023_1;
+
+public class ModelClass {
+    int age = 10;
+    int name;
+
+    public int modelFunction(int total) {
+        int increment = 90;
+        int sum = total + age + increment;
+        return sum;
+    }
+
+}
